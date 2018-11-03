973602789b96c7f1f58b82480fd131504ad7a646
ppbra <kaka020998@gmail.com>
2018-11-03 22:48:39 +0700

    done

--- index.js
+++ index.js
@@ -12,7 +12,50 @@ app.get('/' , (req , res)=>{
     res.render("home.ejs");
 })
 
+const users = [
+    {
+        id:1,
+        userName:"PPbra"
+    },
+    {
+        id:2,
+        userName:"Phuongngo"
+    },
+    {
+        id:3,
+        userName:"Trawnnu"
+    },
+    {
+        id:4,
+        userName:"HELO"
+    },
+] 
 
-io.on("connection",()=>{
+
+io.on("connection",(socket)=>{
     console.log("hello :V");
+    socket.on("client-send-userName",(data)=>{
+        console.log(data);
+       const isContain =  users.find((e)=>{
+            return e.userName===data;
+        })
+
+        if(isContain){
+            socket.emit("register-false");
+        }
+        else{
+            const newUser = {
+                id:users.length+1,
+                userName:data
+            };
+            users.push(newUser);
+            socket.emit("register-success",{users,isYour:newUser});
+            socket.broadcast.emit("new-user",newUser);
+        }
+    });
+    socket.on("log-out",(user)=>{
+        users.splice(users.indexOf(user),1);
+        console.log(users);
+        io.emit("log-out" ,users);
+    })
 })
